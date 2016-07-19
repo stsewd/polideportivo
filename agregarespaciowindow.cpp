558662f92c039c446b0b4739932f359b39ef855f
LaraFranklin <larafranklin95@gmail.com>
2016-07-19 12:51:29 -0500

    reservadao

--- agregarespaciowindow.cpp
+++ agregarespaciowindow.cpp
@@ -12,3 +12,15 @@ AgregarEspacioWindow::~AgregarEspacioWindow()
 {
     delete ui;
 }
+
+void AgregarEspacioWindow::on_cmbTipoEspacio_currentIndexChanged(int index)
+{
+    QStringList listaEspaciosDeportivos=(QStringList()<<"Cancha de Tenis"<<"Cancha de Futbol"<<"Cancha Sintética"<<"Cancha de Voley"<<"Estadio"<<"Piscina");
+    QStringList listaEspaciosComplementarios=(QStringList()<<"Bar"<<"Sala de Conferencias"<<"Sala de Prensa"<<""<<"Tienda Deportiva");
+    if(ui->cmbTipoEspacio->currentText().toUtf8().constData() == "Deportivo"){
+        ui->cmbTipo->addItems(listaEspaciosDeportivos);
+    }else if(ui->cmbTipoEspacio->currentText().toUtf8().constData() == "Deportivo"){
+        ui->cmbTipo->addItems(listaEspaciosComplementarios);
+    }
+
+}
